2c972e8cc65d38bfcf0f3f4dc075affb9366e870
menna1210 <mennaassem1210@gmail.com>
2025-01-30 14:59:04 +0200

    new commit

---
+++ The latest clock.js
@@ -0,0 +1,7 @@
+//Write a function which receives 4 digits and returns the latest time of day that can be built with those digits.
+
+//The time should be in HH:MM format.
+
+function getDigit(arr){
+    let result = 
+}
